0c37ffd4d518df8e317c16ad388feaef468679e1
qtenebrae <serega.ivanovv2012@gmail.com>
2023-07-16 14:46:58 +0300

    The basic functionality of the application has been implemented
    Added the ability to save a city
    Added beautiful weather output to the console

--- services/api.service.js
+++ services/api.service.js
@@ -2,8 +2,32 @@ import https from "https";
 import { TOKEN_DICTIONARY, getKeyValue } from "./storage.service.js";
 import axios from "axios";
 
+const getIcon = (icon) => {
+  switch (icon.slice(0, -1)) {
+    case "01":
+      return "☀️";
+    case "02":
+      return "🌥️";
+    case "03":
+      return "☁️";
+    case "04":
+      return "☁️";
+    case "09":
+      return "🌦️";
+    case "10":
+      return "🌧️";
+    case "11":
+      return "⛈️";
+    case "13":
+      return "❄️";
+    case "50":
+      return "🌫️";
+  }
+};
+
 const getWeather = async (city) => {
-  const token = await getKeyValue(TOKEN_DICTIONARY.token);
+  const token =
+    process.env.TOKEN ?? (await getKeyValue(TOKEN_DICTIONARY.token));
   if (!token) {
     throw new Error(
       "Не задан ключ API, задайте его через команду -t [API_KEY]"
@@ -40,4 +64,4 @@ const getWeather = async (city) => {
   //   });
 };
 
-export { getWeather };
+export { getWeather, getIcon };
--- weather.js
+++ weather.js
@@ -1,7 +1,12 @@
 #!/usr/bin/evn node
 import { getArgs } from "./helpers/args.js";
-import { getWeather } from "./services/api.service.js";
-import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import { getWeather, getIcon } from "./services/api.service.js";
+import {
+  printHelp,
+  printSuccess,
+  printError,
+  printWeather,
+} from "./services/log.service.js";
 import {
   getKeyValue,
   saveKeyValue,
@@ -21,18 +26,47 @@ const saveToken = async (token) => {
   }
 };
 
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError("Не передан город");
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess("Город сохранен");
+  } catch (e) {
+    printError(e.message);
+  }
+};
+
+const getForcast = async () => {
+  try {
+    const city = process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city));
+    const weather = await getWeather(city);
+    printWeather(weather, getIcon(weather.weather[0].icon));
+  } catch (e) {
+    if (e?.response?.status == 404) {
+      printError("Неверно указан город");
+    } else if (e?.response?.status == 401) {
+      printError("Неверно указан токен");
+    } else {
+      printError(e.message);
+    }
+  }
+};
+
 const initCLI = () => {
   const args = getArgs(process.argv);
   if (args.h) {
-    printHelp();
+    return printHelp();
   }
   if (args.s) {
-    // Сохранить город
+    return saveCity(args.s);
   }
   if (args.t) {
     return saveToken(args.t);
   }
-  getWeather("moscow");
+  return getForcast();
 };
 
 initCLI();
